3752481d891144282e390052932e3f34d0ccbe65
adipop425-tech <adipop425@gmail.com>
2026-07-07 19:34:50 +0300

    Creare baza de date cu tabel users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nume');
+            $table->string('prenume');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->enum('role', ['admin', 'operator', 'contabil'])->default('operator');
         });
+
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'nume' => 'admin',
+            'prenume' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('a123'), // Replace with your desired password
+            'role' => 'admin'
         ]);
     }
 }
