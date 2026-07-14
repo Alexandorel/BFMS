36a84eb1d104a10d5dc2163098646243c3696e3c
Alex <vintalexandru03@gmail.com>
2026-07-14 15:00:32 +0300

    dashboard function in OperatorController for extracting the first company of the user

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class OperatorController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        // User with ID = 1, untill login is done
+        $user = User::find(1);
+
+        // First company of the user
+        $company = $user->companies()->first();
+
+        return view('operator.dashboard', [
+            'user' => $user,
+            'company' => $company,
+        ]);
+    }
 }
